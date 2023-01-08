427976e561c5c446385045c2603257a1351658f6
W0L1D <ahdoufwalid@gmail.com>
2023-01-08 21:23:25 +0100

    init commit initial empty demo project

---
+++ src/main/java/com/ilisi2/demotdd/Ilisi2GlUmlDemoTddApplication.java
@@ -0,0 +1,13 @@
+package com.ilisi2.demotdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ilisi2GlUmlDemoTddApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(Ilisi2GlUmlDemoTddApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/ilisi2/demotdd/Ilisi2GlUmlDemoTddApplicationTests.java
@@ -0,0 +1,29 @@
+package com.ilisi2.demotdd;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class Ilisi2GlUmlDemoTddApplicationTests {
+
+   @LocalServerPort
+   private int port;
+   @Autowired
+   private TestRestTemplate restTemplate;
+
+   @Test
+   void contextLoads() {
+   }
+
+
+}
